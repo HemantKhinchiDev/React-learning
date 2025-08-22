eb115ec12d08cea7aee45bfb609aaaf7ccf1f04a
null <null>
2025-08-22 18:02:26 +0530

    Todo Task

---
+++ React-project/src/days/Day1/Day1-practice/Todo.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export const Todo = () => {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+  return (
+    <>
+      Create Your Todo
+      <br />
+      <input
+        type="text"
+        placeholder="Enter task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          if (task.trim() !== "") {
+            setTaskList([...taskList, task]);
+            setTask("");
+          }
+        }}
+      >
+        Add Task
+      </button>
+
+      <ul>
+        {taskList.map((item, index) => (
+          <li key={index}>{item} <button onClick={()=>setTaskList(taskList.filter((_, i) => i !== index))}>❌</button></li>
+        ))}
+      </ul>
+    </>
+  );
+};
--- React-project/src/days/Day1/Day1.jsx
+++ React-project/src/days/Day1/Day1.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import { CounterButton } from "./CounterButton";
 import { MirrorInput } from "./MirrorInput";
 import { FruitList } from "./FruitList";
+import { Todo } from "./Day1-practice/Todo";
 export const Day1 = (props) => {
     const fruits = ["🍎 Apple", "🍌 Banana", "🥭 Mango"];
     return (
         <>
             <div className="">
-                Day 1 Component 
+                Day 1 Learning Component 
+                <CounterButton/>
+                <MirrorInput/>
+                <FruitList fruits={fruits}/>
             </div>
-            <CounterButton/>
-            <MirrorInput/>
-            <FruitList fruits={fruits}/>
+            <div className="">
+                Day 1 Preactice Component  Todo
+                <Todo/> 
+            </div>
+            
         </>
     );
 }
