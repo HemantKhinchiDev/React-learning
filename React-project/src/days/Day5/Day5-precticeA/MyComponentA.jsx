dcc6601c10ee12780374632a8b48b40d3769a9e3
hmnt07 <70513827+HemantKhinchiDev@users.noreply.github.com>
2025-08-29 08:56:47 +0000

    UseRef 2nd example

--- React-project/src/days/Day5/Day5-precticeA/MyComponentA.jsx
+++ React-project/src/days/Day5/Day5-precticeA/MyComponentA.jsx
@@ -1,22 +1,20 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef } from "react";
 
-export const MyComponentA = (prop) => {
-  const [inputText, setInputText] = useState("");
+export const MyComponentA = () => {
+  const countRef = useRef(0);
+  return (
+    <>
+      <h1>Hemant</h1>
 
-  // 1. useRef Hook:
-  // We create a ref to hold a reference to the input DOM element.
-  // We initialize it with null.
-  const inputRef = useRef(null);
-
-  // 2. useEffect Hook:
-  // This hook handles the side effect of focusing the input element.
-  useEffect(() => {
-    // Check if the current property of the ref exists and then call the focus() method on it.
-    // This code runs only once when the component is mounted due to the empty dependency array.
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, []); // The empty dependency array `[]` ensures this effect runs only once.
-
-  return <></>;
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          countRef.current = countRef.current + 1;
+          console.log(countRef.current);
+        }}
+      >
+        Click Me & check count in console
+      </button>
+    </>
+  );
 };
--- React-project/src/days/Day5/Day5.jsx
+++ React-project/src/days/Day5/Day5.jsx
@@ -1,12 +1,13 @@
 import { MyComponent } from "./Day5-prectice/MyComponent";
 import { MyComponentA } from "./Day5-precticeA/MyComponentA";
+
 export const Day5 = () => {
   return (
     <>
       Day5
       <h2 title={"ऑटो-फोकस इनपुट"}></h2>
       <MyComponent />
-      <MyComponetA />
+      <MyComponentA />
     </>
   );
 };
